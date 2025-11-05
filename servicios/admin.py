5af85ac84e3989785d9e0a1d3d33e648662f6ab6
luisma <mrluisgamerps3@gmail.com>
2025-11-05 00:08:16 -0400

    addinng more stuff to the admin and html services

--- servicios/admin.py
+++ servicios/admin.py
@@ -3,6 +3,25 @@ from django.contrib import admin
 from .models import Servicio
 from django.utils.html import format_html
 
+class RangoPrecioFilter(admin.SimpleListFilter):
+    title = 'Rango de precios'  # Texto que aparece en el panel lateral
+    parameter_name = 'rango_precio'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('bajo', 'Menos de $100'),
+            ('medio', 'Entre $100 y $500'),
+            ('alto', 'Más de $500'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'bajo':
+            return queryset.filter(precio__lt=100)
+        if self.value() == 'medio':
+            return queryset.filter(precio__gte=100, precio__lte=500)
+        if self.value() == 'alto':
+            return queryset.filter(precio__gt=500)
+        return queryset
 
 @admin.register(Servicio)
 class ServicioAdmin(admin.ModelAdmin):
@@ -19,6 +38,7 @@ class ServicioAdmin(admin.ModelAdmin):
         'disponible',
         'precio',  # Rango de precios
         'fecha_creacion',
+        RangoPrecioFilter
     )
     search_fields = ('titulo', 'descripcion')
     list_editable = ('disponible',)
