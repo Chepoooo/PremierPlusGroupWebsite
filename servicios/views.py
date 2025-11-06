19055760fd482366915a9feb7c28815c9dc1df93
luisma <mrluisgamerps3@gmail.com>
2025-11-06 00:29:12 -0400

    fixing mistakes

--- servicios/views.py
+++ servicios/views.py
@@ -6,17 +6,29 @@ def home(request):
     return render(request, 'home.html')
 
 def vehiculos(request):
-    servicios = Servicio.objects.filter(categoria='vehiculo', disponible=True)
-    return render(request, 'vehiculos.html', {'servicios': servicios})
+    servicios_list = Servicio.objects.filter(categoria='vehiculo', disponible=True)
+    paginator = Paginator(servicios_list, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'vehiculos.html', {'page_obj': page_obj})
 
 def apartamentos(request):
-    servicios = Servicio.objects.filter(categoria='apartamento', disponible=True)
-    return render(request, 'apartamentos.html', {'servicios': servicios})
+    servicios_list = Servicio.objects.filter(categoria='apartamento', disponible=True)
+    paginator = Paginator(servicios_list, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'apartamentos.html', {'page_obj': page_obj})
 
 def botes(request):
-    servicios = Servicio.objects.filter(categoria='bote', disponible=True)
-    return render(request, 'botes.html', {'servicios': servicios})
+    servicios_list = Servicio.objects.filter(categoria='bote', disponible=True)
+    paginator = Paginator(servicios_list, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'botes.html', {'page_obj': page_obj})
 
 def paquetes(request):
-    servicios = Servicio.objects.filter(categoria='paquete', disponible=True)
-    return render(request, 'paquetes.html', {'servicios': servicios})
+    servicios_list = Servicio.objects.filter(categoria='paquete', disponible=True)
+    paginator = Paginator(servicios_list, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'paquetes.html', {'page_obj': page_obj})
